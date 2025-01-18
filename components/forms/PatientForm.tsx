80c8c2cc1a55dca4b6071634e37c1e8e795d504c
Rajpreet0 <rajpreet.singh@gmx.de>
2025-01-18 15:42:41 +0100

    added appwrite logic

--- components/forms/PatientForm.tsx
+++ components/forms/PatientForm.tsx
@@ -10,6 +10,7 @@ import SubmitButton from "../SubmitButton";
 import { useState } from "react";
 import { UserFormValidation } from "@/lib/validation";
 import { useRouter } from "next/navigation";
+import { createUser } from "@/lib/actions/patient.actions";
 
 export enum FormFieldType {
   INPUT = 'input',
@@ -47,7 +48,9 @@ const PatientForm = () => {
         phone,
       }
 
+      const user = await createUser(userData);
 
+      if(user) router.push(`/patients/${user.$id}/register`);
 
     } catch (error) {
       console.log(error);
