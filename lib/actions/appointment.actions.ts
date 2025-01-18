d2ccaa1b3c462866bfb8307f18f0e695c0ca7ca5
Rajpreet0 <rajpreet.singh@gmx.de>
2025-01-18 19:27:41 +0100

    added success appointment page

--- lib/actions/appointment.actions.ts
+++ lib/actions/appointment.actions.ts
@@ -3,6 +3,7 @@
 import { ID } from "node-appwrite";
 import { APPOINTMENT_COLLECTION_ID, DATABASE_ID, databases } from "../appwrite.config";
 import { parseStringify } from "../utils";
+import { stringify } from "querystring";
 
 
 export const createAppointment = async (appointment: CreateAppointmentParams) => {
@@ -20,4 +21,19 @@ export const createAppointment = async (appointment: CreateAppointmentParams) =>
         console.log("Error occured here!")
     }
 
+}
+
+
+export const getAppointment = async (appointmentId: string) => {
+    try {
+        const appointment = await databases.getDocument(
+            DATABASE_ID!,
+            APPOINTMENT_COLLECTION_ID!,
+            appointmentId,
+        )
+
+        return parseStringify(appointment);
+    } catch (error) {
+        console.log(error);
+    }
 }
